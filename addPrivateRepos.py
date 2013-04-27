e234fb2893b56f2ce730fed790b8a848c2d13d20
Selena Deckelmann <selenamarie@gmail.com>
2013-04-26 18:59:33 -0700

    Added list_repos(), create_config() and argparse options

--- addPrivateRepos.py
+++ addPrivateRepos.py
@@ -1,5 +1,11 @@
 from pygithub3 import Github
-from secrets import *
+
+try:
+    from secrets import *
+except:
+    print "Need to specify secrets! Try giving your username and password."
+
+import argparse
 
 """
  README
@@ -14,56 +20,104 @@ from secrets import *
 	ORG="AdaInitiative"
 """
 
-# ADD YOUR LIST OF NAMES HERE
-# TODO: make this better, I'll set up argparse in v0.2 unless someone has a better idea
-usernames = ["username1", "username2"]
+def create_config(args):
 
-gh = Github(login=USERNAME, password=PASSWORD)
-org = ORG
+    f = open('secrets.py', 'w')
+    f.write("""
+USERNAME="%s"  # must be an AI owner to run this script
+PASSWORD="%s"
+TEAM_ID=393250  # privacyfans
+OWNERS=360990	# owners team id
+ORG="AdaInitiative"
+""" % (args.username, args.password))
+    f.close()
+    print "Created your secrets.py file"
 
-team_names = []
-pf_names = []
-repo_names = []
+def connect():
+    gh = Github(login=USERNAME, password=PASSWORD)
+    return gh
 
-existing_teams = gh.orgs.teams.list(org).all()
-for team in existing_teams:
-	team_names.append(str(team.name))
-privacyfans = gh.orgs.teams.list_members(TEAM_ID).all()
-for p in privacyfans:
-	pf_names.append(str(p.login))
-repos = gh.orgs.teams.list_repos(OWNERS).all()
-for r in repos:
-	repo_names.append(str(r.name))
+def list_repos():
+    gh = connect()
+    org = ORG
+    existing_teams = gh.orgs.teams.list(org).all()
+    for team in existing_teams:
+        print team.name
 
-for user in usernames:
-	data = {
-		  "name": user ,
-		  "permission": "admin",
-	}
-	# Step 1 - add users to team 'privacyfans' if not already
-	if user not in pf_names:
-		gh.orgs.teams.add_member(TEAM_ID,user)
-		print "Added: %s to 'privacyfans' team" % user
-	else:
-		print "%s already in team privacyfans" % user
+def create_repos(args):
+    # ADD YOUR LIST OF NAMES HERE
+    # TODO: make this better, I'll set up argparse in v0.2 unless someone has a better idea
 
-	# Step 2 - create a team of the user's name if not already
-	if user not in team_names:
-		team = gh.orgs.teams.create(org, data)
-		print "Created a team for %s in %s" % (user, org)
-	else:
-		print "Team %s already exists" % user
+    usernames = args.newaccounts
 
-	# Step 3 - create a repo of the user's name if not already
-	if user not in repo_names:
-		gh.repos.create(dict(name=user, team_id=team.id, private=True, auto_init=True),
-	    in_org=org)
-		print "Created a repo for %s in %s" % (user, org)
-	else:
-		print "Repo %s already exists" % user
+    gh = connect()
+    org = ORG
 
-	# Add the member into the team
-	gh.orgs.teams.add_member(team.id, user)
-	print "Added %s to team %s" % (user,user)
+    team_names = []
+    pf_names = []
+    repo_names = []
 
-print "\n== ALL DONE! =="
+    existing_teams = gh.orgs.teams.list(org).all()
+    for team in existing_teams:
+        team_names.append(str(team.name))
+    privacyfans = gh.orgs.teams.list_members(TEAM_ID).all()
+    for p in privacyfans:
+        pf_names.append(str(p.login))
+    repos = gh.orgs.teams.list_repos(OWNERS).all()
+    for r in repos:
+        repo_names.append(str(r.name))
+
+    for user in usernames:
+        data = {
+              "name": user ,
+              "permission": "admin",
+        }
+        # Step 1 - add users to team 'privacyfans' if not already
+        if user not in pf_names:
+            gh.orgs.teams.add_member(TEAM_ID,user)
+            print "Added: %s to 'privacyfans' team" % user
+        else:
+            print "%s already in team privacyfans" % user
+
+        # Step 2 - create a team of the user's name if not already
+        if user not in team_names:
+            team = gh.orgs.teams.create(org, data)
+            print "Created a team for %s in %s" % (user, org)
+        else:
+            print "Team %s already exists" % user
+
+        # Step 3 - create a repo of the user's name if not already
+        if user not in repo_names:
+            gh.repos.create(dict(name=user, team_id=team.id, private=True, auto_init=True),
+            in_org=org)
+            print "Created a repo for %s in %s" % (user, org)
+        else:
+            print "Repo %s already exists" % user
+
+        # Add the member into the team
+        gh.orgs.teams.add_member(team.id, user)
+        print "Added %s to team %s" % (user,user)
+
+    print "\n== ALL DONE! =="
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Add a repo to git')
+    parser.add_argument('--newaccounts', nargs='+',
+                       help='new account username(s) you are adding a private repo for')
+    parser.add_argument('--username',
+                       help='your github username')
+    parser.add_argument('--password',
+                       help='your github password')
+    parser.add_argument('--list_repos', action='store_true',
+                       help='list existing repos')
+
+    args = parser.parse_args()
+
+    if args.username and args.password:
+        create_config(args)
+    elif args.newaccounts:
+        create_repos(args)
+    elif args.list_repos:
+        list_repos()
+    else:
+        print "Nothing to do!"
